993304ff572617cb9d0d3aa2f3dc376f59f53200
임승현 <126482821+limsbong@users.noreply.github.com>
2023-07-10 15:15:25 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -2,11 +2,11 @@ function solution(money) {
     let answer = [];
     let piece = 0;
     let CutMoney = 0;
-    if(money >= 0) {
+    
         piece = Math.floor(money / 5500);
         answer.push(piece);
         CutMoney = money - (piece * 5500)
         answer.push(CutMoney);
-    }
+    
     return answer;
 }
