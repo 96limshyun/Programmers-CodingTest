791a9ceab7a645fbdf738b83464fabd5b96fd51a
임승현 <126482821+96limshyun@users.noreply.github.com>
2024-07-17 22:52:36 +0900

    [Bronze II] Title: 분산처리, Time: 160 ms, Memory: 10276 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.js
+++ 백준/Bronze/1009. 분산처리/분산처리.js
@@ -1,21 +1,33 @@
-const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const count = Number(input.splice(0, 1)[0]);
 
-let N = input.shift();
+const patternsMap = {
+    10: [10],
+    1: [1],
+    2: [6, 2, 4, 8],
+    3: [1, 3, 9, 7],
+    4: [6, 4],
+    5: [5],
+    6: [6],
+    7: [1, 7, 9, 3],
+    8: [6, 8, 4, 2],
+    9: [1, 9]
+};
 
-const result = input.map((v) => {
-    const [a, b] = v.split(" ");
-
-    let pow = 1;
-
-    for (let j = 0; j < b; j++) {
-        pow = (pow * a) % 10;
+const main = () => {
+    if (count === input.length) {
+        for (let i = 0; i < input.length; i++) {
+            const [a, b] = input[i].split(" ").map(Number);
+            const A = a % 10 === 0 ? 10 : a % 10;
+            const curPatterns = patternsMap[A];
+            const B = b % curPatterns.length;
+            const result = curPatterns[B];
+            console.log(result);
+        }
+    } else {
+        console.log("Input length mismatch");
     }
+};
 
-    return pow === 0 ? 10 : pow;
-});
-
-console.log(result.join("\n"));
+main();
