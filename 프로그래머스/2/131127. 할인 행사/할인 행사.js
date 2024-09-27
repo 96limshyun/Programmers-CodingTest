0b251c6f8bb520c4adc52ba7c6f23c2918bcf597
임승현 <126482821+96limshyun@users.noreply.github.com>
2024-09-27 20:41:49 +0900

    [level 2] Title: 할인 행사, Time: 22.29 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,17 +1,19 @@
 function solution(want, number, discount) {
     let count = 0;
-    for (let i = 0; i < discount.length - 9; i++) {
-        const slice = discount.slice(i, i + 10)
-
-        let flag = true
+    for(let i = 0; i < discount.length - 9; i++) {
+        let flag = true;
+        const arr = discount.slice(i, i + 10)
+        
         for(let j = 0; j < want.length; j++) {
-            const filter = slice.filter((curMap, idx) => want[j] === curMap)
-            if(filter.length < number[j]) {
-                flag = false;
+            const a = arr.filter(cur => cur === want[j])
+            if(a.length !== number[j]){
+                flag = false
                 break;
             }
         }
-        if(flag) count++
+        if(flag) {
+            count++
+        }
     }
     return count
 }
