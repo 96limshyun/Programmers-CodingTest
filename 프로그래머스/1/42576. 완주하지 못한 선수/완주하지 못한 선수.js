db3ee975f1426a10ad043d28447f5197f7f63e90
임승현 <126482821+96limshyun@users.noreply.github.com>
2024-09-28 00:01:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.39 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,17 @@
 function solution(participant, completion) {
-    participant.sort()
-    completion.sort()
-    for(let i = 0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]) {
-            return participant[i]
+    const map = new Map()
+    
+    participant.forEach(cur => {
+        map.set(cur, (map.get(cur) || 0 ) + 1)
+    })
+
+    for (let name of completion) {
+        map.set(name, (map.get(name) || 0 ) - 1)
+    }
+
+    for (let [key, value] of map) {
+        if(value > 0){
+            return key
         }
     }
 }
