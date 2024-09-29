fe58c864b9803991a74ff81ea958811d8a23863d
임승현 <126482821+96limshyun@users.noreply.github.com>
2024-09-29 15:36:37 +0900

    [level 2] Title: 오픈채팅방, Time: 267.27 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,30 +1,23 @@
 function solution(record) {
     const userMap = new Map()
     const actionMap = [];
-    const result = [];
+    const message = {
+        "Enter": "님이 들어왔습니다.",
+        "Leave": "님이 나갔습니다.",
+    }
     
     record.forEach(curUser => {
-        const user = curUser.split(" ")
+        const [action, id, nick] = curUser.split(" ")
         
-        actionMap.push({id: user[1], action: user[0]})
-        
-        if(user.length === 2) return
-        const nickname = user[2]
-        
-        userMap.set(user[1], user[2])
-    })
-    
-    actionMap.forEach((curUser, idx) => {
-        const nickname = userMap.get(curUser.id)
-        const action = curUser.action
-        switch(action) {
-            case "Enter":
-                result.push(`${nickname}님이 들어왔습니다.`)
-                break;
-            case "Leave":
-                result.push(`${nickname}님이 나갔습니다.`)
-                break;
+        if(action !== "Change") {
+            actionMap.push([action, id])        
+        }
+        if(nick){
+            userMap.set(id, nick)
         }
     })
-    return result
+    
+    return actionMap.map(([action, id]) => {
+        return `${userMap.get(id)}${message[action]}`
+    })
 }
